5d4260646cb0bb21c962155f2c44db0f3602a8c5
Brighton Malingi <edead959@gmail.com>
2026-08-22 12:06:53 +0300

    test(candyai): cover inbox autonomous gating

--- spec/listeners/candy_ai_listener_spec.rb
+++ spec/listeners/candy_ai_listener_spec.rb
@@ -6,11 +6,7 @@ RSpec.describe CandyAIListener do
   subject(:listener) { described_class.instance }
 
   let(:config) do
-    instance_double(
-      CandyAI::Configuration,
-      enabled: true,
-      default_ai_provider: 'openai'
-    )
+    instance_double(CandyAI::Configuration, enabled: true)
   end
   let(:message) do
     instance_double(
@@ -18,22 +14,47 @@ RSpec.describe CandyAIListener do
       id: 42,
       incoming?: true,
       private?: false,
-      content_for_llm: 'Hello'
+      content_for_llm: 'Hello',
+      inbox: inbox
     )
   end
-  let(:account) { instance_double(Account) }
+  let(:inbox) { instance_double(Inbox) }
   let(:event) { instance_double(Events::Base, data: { message: message }) }
 
   before do
     allow(CandyAI).to receive(:config).and_return(config)
-    allow(ENV).to receive(:[]).and_call_original
-    allow(ENV).to receive(:[]).with('CANDYAI_AGENT_BOT_ID').and_return('123')
+    allow(CandyAI::AccountConfiguration).to receive(:effective).with(inbox).and_return(inbox_configuration)
   end
 
-  it 'queues an AI response for an eligible incoming message' do
-    allow(message).to receive(:account).and_return(account)
+  let(:inbox_configuration) do
+    {
+      'enabled' => true,
+      'mode' => 'autonomous'
+    }
+  end
 
+  it 'queues an AI response for an enabled autonomous inbox' do
     expect(CandyAI::RespondToMessageJob).to receive(:perform_later).with(42)
+
+    listener.message_created(event)
+  end
+
+  it 'does not queue responses for assist mode' do
+    allow(CandyAI::AccountConfiguration).to receive(:effective).with(inbox).and_return(
+      inbox_configuration.merge('mode' => 'assist')
+    )
+
+    expect(CandyAI::RespondToMessageJob).not_to receive(:perform_later)
+
+    listener.message_created(event)
+  end
+
+  it 'does not queue responses for a disabled inbox' do
+    allow(CandyAI::AccountConfiguration).to receive(:effective).with(inbox).and_return(
+      inbox_configuration.merge('enabled' => false)
+    )
+
+    expect(CandyAI::RespondToMessageJob).not_to receive(:perform_later)
 
     listener.message_created(event)
   end
