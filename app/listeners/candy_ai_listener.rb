cecf66194f528b9b2e638e05c685d24038907837
Brighton Malingi <edead959@gmail.com>
2026-08-22 12:06:17 +0300

    feat(candyai): gate autonomous responses by inbox configuration

--- app/listeners/candy_ai_listener.rb
+++ app/listeners/candy_ai_listener.rb
@@ -9,17 +9,21 @@ class CandyAIListener < BaseListener
     message = extract_message_and_account(event)[0]
     return unless eligible_message?(message)
 
+    configuration = CandyAI::AccountConfiguration.effective(message.inbox)
+    return unless configuration['enabled'] == true
+    return unless configuration['mode'] == 'autonomous'
+
     CandyAI::RespondToMessageJob.perform_later(message.id)
   end
 
   private
 
   def eligible_message?(message)
+    return false if message.blank?
     return false unless message.incoming?
     return false if message.private?
     return false if message.content_for_llm.blank?
-    return false unless CandyAI.config.default_ai_provider.present?
-    return false unless ENV['CANDYAI_AGENT_BOT_ID'].present?
+    return false unless message.respond_to?(:inbox) && message.inbox.present?
 
     true
   end
