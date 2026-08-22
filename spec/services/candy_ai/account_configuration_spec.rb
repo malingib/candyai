8faac5797f854966efb715861e1069aed689d4bb
Brighton Malingi <edead959@gmail.com>
2026-08-22 12:07:39 +0300

    test(candyai): verify account kill switch and effective config

--- spec/services/candy_ai/account_configuration_spec.rb
+++ spec/services/candy_ai/account_configuration_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe CandyAI::AccountConfiguration do
   describe '.account' do
     it 'merges persisted account settings over defaults' do
       account = build(:account, settings: { 'candy_ai' => { 'enabled' => true, 'model' => 'test-model' } })
-
       configuration = described_class.account(account)
 
       expect(configuration['enabled']).to be(true)
@@ -34,6 +33,41 @@ RSpec.describe CandyAI::AccountConfiguration do
       configuration = described_class.normalize_inbox('mode' => 'invalid')
 
       expect(configuration['mode']).to eq('assist')
+    end
+  end
+
+  describe '.effective' do
+    let(:account) do
+      build(:account, settings: {
+        'candy_ai' => {
+          'enabled' => true,
+          'provider' => 'openai',
+          'model' => 'account-model'
+        }
+      })
+    end
+    let(:inbox) do
+      build(:inbox, account: account, candy_ai_settings: {
+        'enabled' => true,
+        'mode' => 'autonomous',
+        'model' => 'inbox-model'
+      })
+    end
+
+    it 'combines account and inbox settings' do
+      effective = described_class.effective(inbox)
+
+      expect(effective['enabled']).to be(true)
+      expect(effective['mode']).to eq('autonomous')
+      expect(effective['provider']).to eq('openai')
+      expect(effective['model']).to eq('inbox-model')
+    end
+
+    it 'cannot bypass an account-level disablement' do
+      account.settings['candy_ai']['enabled'] = false
+      effective = described_class.effective(inbox)
+
+      expect(effective['enabled']).to be(false)
     end
   end
 end
